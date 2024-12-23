df038128f58cf1b4c558ec4f7044b782be03c104
Shubham <marutibross@gmail.com>
2024-12-24 01:35:29 +0530

    learned abput the zod validation

--- middlewareUsingHospitalRoutes.js
+++ middlewareUsingHospitalRoutes.js
@@ -2,26 +2,51 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+const zod = require("zod");
 
-app.get("/health-checkup" , (req,res) => {
- if(req.headers.id  != "Shubham" || req.headers.pass !== "pass"){
-    res.status(500).json({msg : "incorrect is pass"})
- }
- if(req.query.kidId != 2 ){
-    res.status(500).json({msg : "something is ur up with kidney"});
+const schema = zod.array(zod.number());
+let requestCount = 0;
+//middleware
+function requestCheck(req, res, next) {
+    requestCount++;
+    console.log(requestCount);
+    next();
 }
- res.status(200).json({msg : "your are healthy....."})
+
+app.get("/health-checkup", (req, res) => {
+    if (req.headers.id != "Shubham" || req.headers.pass !== "pass") {
+        res.status(500).json({ msg: "incorrect is pass" })
+    }
+    if (req.query.kidId != 2) {
+        res.status(500).json({ msg: "something is ur up with kidney" });
+    }
+    res.status(200).json({ msg: "your are healthy....." })
 })
 
-app.get("/kidney-check" , (req,res) => {
+app.get("/kidney-check", (req, res) => {
 
-    if(req.query.kidId != 2 ){
-        res.status(500).json({msg : "something is ur up with kidney"});
+    if (req.query.kidId != 2) {
+        res.status(500).json({ msg: "something is ur up with kidney" });
     }
 
-    res.status(200).json({msg : "All good"});
+    res.status(200).json({ msg: "All good" });
 })
 
-app.listen(3000, ()=> {
-   console.log("Server is started at port 3000");
+app.listen(3000, () => {
+    console.log("Server is started at port 3000");
+})
+
+app.post('/health-checkup', (req, res) => {
+    const kidney = req.body.kidneyLength;
+    const response = schema.safeParse(kidney);
+    if (!response.success) {
+        res.status(500).json({
+            msg: "invalid input"
+        })
+    }
+    else {
+        res.json({
+            response
+        });
+    }
 })
